cac0aa7b6b19e100e42a0312efd90aa3b5f877d5
Hadnan Basilio <basiliohadnan@gmail.com>
2024-03-28 13:23:01 -0300

    Adding my suggestion of changes: 1. preferred to use regex 2. removed DivisionByZero's custom exception. 3. removed unnecessary print

---
+++ forth.py
@@ -0,0 +1,59 @@
+import inspect, re
+from itertools import chain
+
+OPS = {
+    '+':    lambda x, y: [y + x],
+    '-':    lambda x, y: [y - x],
+    '*':    lambda x, y: [y * x],
+    '/':    lambda x, y: [y // x],
+    'dup':  lambda x:    [x, x],
+    'drop': lambda _:    [],
+    'swap': lambda x, y: [x, y],
+    'over': lambda x, y: [y, x, y],
+}
+
+class StackUnderflowError(Exception):
+    def __init__(self):
+        super().__init__('Insufficient number of items in stack')
+        
+#removed DivisionByZero's custom exception.
+        
+def is_number(elem):
+#preferred to use regex
+    return bool(re.match(r'^-?\d+(\.\d+)?$', elem))
+
+def apply(stack, elem):
+    if is_number(elem):
+        stack.append(int(elem))
+    elif elem in OPS:
+        op = OPS[elem]
+        count = len(inspect.signature(op).parameters)
+        if len(stack) < count:
+            #removed unnecessary print
+            raise StackUnderflowError
+        stack.extend(op(*(stack.pop() for x in range(count))))
+    else:
+        raise ValueError('undefined operation')
+
+def substitute(custom, elems):
+    return list(chain(*(custom[x] if x in custom else [x] for x in elems)))
+
+def evaluate(input_data):
+    stack = []
+    custom = {}
+    try:
+        for line in input_data:
+            elems = line.lower().split()
+            if elems[0] == ':':
+                assert elems[-1] == ';'
+                op = elems[1]
+                if is_number(op):
+                    raise ValueError('illegal operation')
+                custom[op] = substitute(custom, elems[2:-1])
+            else:
+                elems = substitute(custom, elems)
+                for elem in elems:
+                    apply(stack, elem)
+        return stack
+    except ZeroDivisionError:
+        raise ZeroDivisionError('divide by zero') 
